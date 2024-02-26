a17d999f89fe20ee949e09f8420457ef5b46e587
Suraj Adhikari <sssadhikari222@gmail.com>
2024-02-26 20:46:03 +0545

    About Us Some Changes

--- JS/index.js
+++ JS/index.js
@@ -11,7 +11,7 @@
 //     }
 // }
 
-
+//Start Index Hero Banners
 document.addEventListener("DOMContentLoaded", function () {
     let currentSlide = 0;
     const slides = document.querySelectorAll(".heroBannerSlides");
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
         setInterval(nextSlide, 9500);
     }, 500);
 
-
+//Start Down and Up Arrow
     document.querySelectorAll('.nav-header-bar').forEach(function(navHeaderBar) {
         navHeaderBar.addEventListener('click', function() {
             var navId = this.dataset.navId;
@@ -51,19 +51,64 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-
+//Start navbar options
     document.getElementById('burger-menu').addEventListener('click', function() {
         this.classList.toggle('open');
         var mobileNavPanel = document.getElementById('mobileNavPanel');
         mobileNavPanel.classList.toggle('open');
     });
       
-
+//Start FQS + and -
     $(document).ready(function () {
         $(".FAQS-title-question").click(function () {
             $(this).next(".question-content-answer").toggleClass("content-is-active");
             $(this).find(".question-plus-icon").find(".Question-toggle-closed, .Question-toggle-open").toggle();
         });
     });
+
+
+//About Infinity Images
+    const leftImages = document.querySelectorAll('.infinity-left-image .infinity-photo-content');
+    const rightImages = document.querySelectorAll('.infinity-right-image .infinity-photo-content');
+
+    function toggleImages(images) {
+      images.forEach((image, index) => {
+        setTimeout(() => {
+          image.style.opacity = '1';
+          setTimeout(() => {
+            image.style.opacity = '0';
+          }, 3000);
+        }, index * 6000); 
+      });
+    }
+
+    function startImageLoop() {
+      setInterval(() => {
+        toggleImages(leftImages);
+        toggleImages(rightImages);
+      }, leftImages.length * 2000);
+    }
+
+    startImageLoop();
+
+    //About Readmore Buttons
+    document.getElementById('readMore').addEventListener('click', function () {
+        const expandHistory = document.querySelector('.expand-history');
+        const readMoreBtn = document.querySelector('.read-more-btn');
+        const downArrow = document.getElementById('downArrowIcon');
+    
+        if (expandHistory.style.height === '100%') {
+            expandHistory.style.height = '6rem';
+            readMoreBtn.style.opacity = '1';
+            readMoreBtn.style.display = 'block';
+            downArrow.style.display = 'block';
+        } else {
+            expandHistory.style.height = '100%';
+            readMoreBtn.style.opacity = '0';
+            readMoreBtn.style.display = 'none';
+            downArrow.style.display = 'none';
+        }
+    });
+    
 
 });
