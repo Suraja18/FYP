7a74727617fee217ee01d62c46fbe5bb691d1874
Suraj Adhikari <sssadhikari222@gmail.com>
2024-03-19 08:31:34 +0545

    Maintainance Request

--- JS/index.js
+++ JS/index.js
@@ -268,6 +268,70 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 
+    //Profile Mainly
+    const avatarButtonOpen = document.getElementById('profileAvatarButton');
+    const avatarProfileOpen = document.getElementById('profileAvatarOpen');
+    if (avatarButtonOpen && avatarProfileOpen) {
+        avatarButtonOpen.addEventListener('click', function() {
+            avatarProfileOpen.classList.toggle('active');
+        });
+    }
+
+
+    // Toggle More Options for Maintenance
+    const btnMaintainanceIcons = document.querySelectorAll('.btn-more-option');
+    const btnMaintainanceIconOptions = document.querySelectorAll('.more-info-for-maintainance');
+
+    btnMaintainanceIcons.forEach((btnMaintainanceIcon, index) => {
+        const btnMaintainanceIconOption = btnMaintainanceIconOptions[index];
+        btnMaintainanceIcon.addEventListener('click', function() {
+            btnMaintainanceIconOption.classList.toggle('active');
+        });
+    });
+
+
+    //Toggle Status Mainly
+    const statusFullWrapper = document.getElementById('statusFullWrapper');
+    const statusFullOption = document.getElementById('statusFullOption');
+
+    if (statusFullWrapper && statusFullOption) {
+        statusFullWrapper.addEventListener('click', function() {
+            statusFullOption.classList.toggle('active');
+        });
+        document.getElementById('statusApply').addEventListener('click', function() {
+            var selectedStatus = document.getElementById('multiScrollSelect').value;
+            var statusSpan = document.getElementById('insideTheBox');
+            statusSpan.innerHTML = '<span class="mr-5p">Status</span><span class="status-filters fw-600">' + selectedStatus + '</span>';
+            var successMessage = document.getElementById('successStatusChange');
+            successMessage.style.display = 'block';
+            setTimeout(function() {
+                successMessage.style.display = 'none';
+            }, 5 * 60 * 1000);
+        });
+        
+    }
+
+
+    //More Options Mainly
+    const navMoreButton = document.getElementById('navMoreButton');
+    const navMoreOptions = document.getElementById('navMoreOptions');
+    if (navMoreButton && navMoreOptions) {
+
+        const svg1 = '<svg xmlns="http://www.w3.org/2000/svg" height="28" width="35" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>';
+        const svg2 = '<svg xmlns="http://www.w3.org/2000/svg" height="28" width="35" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>';
+        let toggle = false;
+
+        navMoreButton.addEventListener('click', function() {
+            toggle = !toggle;
+            if (toggle) {
+                navMoreButton.innerHTML = svg2 + '<p class="small-text">More</p>';
+            } else {
+                navMoreButton.innerHTML = svg1 + '<p class="small-text">More</p>';
+            }
+            navMoreOptions.classList.toggle('active');
+            navMoreButton.classList.toggle('active');
+        });
+    }
 
 });
 
